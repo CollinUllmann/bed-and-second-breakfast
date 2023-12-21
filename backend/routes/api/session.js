c45ece61f6550125ed988708471e958091a025a0
Collin <collinullmann@gmail.com>
2023-12-21 16:56:56 -0500

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -42,8 +42,23 @@ router.post('/', async (req, res, next) => {
 );
 
 router.delete('/', (_req, res) => {
-  res.clearCookie('XSRF-TOKEN');
+  res.clearCookie('token');
   return res.json({ message: 'success' });
+}
+);
+
+router.get('/', (req, res) => {
+  const { user } = req;
+  if (user) {
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    return res.json({
+      user: safeUser
+    });
+  } else return res.json({ user: null });
 }
 );
 
