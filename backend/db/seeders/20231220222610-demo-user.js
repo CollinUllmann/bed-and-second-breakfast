70c0c0339491430b4a81e62a7573fc5189852a3f
Collin <collinullmann@gmail.com>
2023-12-22 14:01:01 -0500

    Added validation features

--- backend/db/seeders/20231220222610-demo-user.js
+++ backend/db/seeders/20231220222610-demo-user.js
@@ -12,16 +12,22 @@ module.exports = {
   async up(queryInterface, Sequelize) {
     await User.bulkCreate([
       {
+        firstName: 'Joe',
+        lastName: 'Bob',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
+        firstName: 'Billy',
+        lastName: 'Boy',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
+        firstName: 'Jane',
+        lastName: 'Doe',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -30,12 +30,14 @@ const validateSignup = [
 const router = express.Router();
 
 router.post('/', validateSignup, async (req, res) => {
-  const { email, password, username } = req.body;
+  const { email, password, username, firstName, lastName } = req.body;
   const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({ email, username, hashedPassword });
+  const user = await User.create({ email, username, hashedPassword, firstName, lastName });
 
   const safeUser = {
     id: user.id,
+    firstName: user.firstName,
+    lastName: user.lastName,
     email: user.email,
     username: user.username,
   };
